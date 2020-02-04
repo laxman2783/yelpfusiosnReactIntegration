1534a7f25e9e1a8926312cc1a17c62d2ac6ed312
laxman <laxman.thota@pwc.com>
2020-02-04 03:59:27 -0500

    Yelp fusion React

---
+++ src/NavBar/SubNav/SubNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './SubNav.module.css';
+
+export function SubNav() {
+    return (
+        <div className={styles.container}>
+            <div className={styles['sub-nav']}>
+                <div>
+                    </div>
+                 </div>
+        </div>
+        
+    );
+}
---
+++ src/hooks/yelp-api/config.js
@@ -0,0 +1,2 @@
+export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
+export const BEARER_TOKEN = 'tC0WfgHLl-U2g4sZPNlfllQrlOuRN4s6H2v1GThE_OCaRZf-0Cvbe2axmdSm7n9xJn4PS847b-jPxq6nQADo30TMVyOL9ABgFQn2a9MB8yXXgc7ELhq2AtSFqXcyXnYx';
